481affcd46eddd2abfac9dee7651b681bf488e18
Nikita German <N1IKITA23@yandex.ru>
2026-01-11 11:46:33 +0300

    style: code style

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,12 +1,8 @@
 package ru.practicum.shareit.item.model;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-/**
- * TODO Sprint add-controllers.
- */
 @Data
 @EqualsAndHashCode(of = {"id"})
 public class Item {
